dc9d4b2baf8ae99f0bd5ba6e4460ccc5c107ea77
Bhuvaneswari k <bhuvaneswari012k@gmail.com>
2026-07-23 20:17:46 +0530

    Create Maximum Consecutive 1's.java

---
+++ Maximum Consecutive 1's.java
@@ -0,0 +1,17 @@
+public static void main(String[] args) {
+     int arr[] = {1, 1, 0, 1, 1, 1, 0, 1};
+     int count=0;
+     int maxcount=0;
+     for(int i=0;i<arr.length;i++){
+      if(arr[i]==1){
+        count++;
+        if(count>maxcount){
+        maxcount=count;
+      }
+      }
+      else{
+      count=0;
+     }
+     }
+     System.out.print(maxcount);
+     }
